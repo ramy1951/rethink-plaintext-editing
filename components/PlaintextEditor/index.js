233aaa2ee5692cd85151b4131e499b2079b6fc8f
Ramy1951 <ramy1951@live.com>
2020-11-05 22:15:34 -0800

    :rocket: PlainTextEditor completed successfully!

--- components/PlaintextEditor/index.js
+++ components/PlaintextEditor/index.js
@@ -1,16 +1,49 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-
+import React, { useEffect, useState } from 'react';
 import css from './style.css';
+import { TextField } from '@material-ui/core'
+
 
 function PlaintextEditor({ file, write }) {
-  console.log(file, write);
+
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      setText(await file.text());
+    })();
+  }, [file]);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+    console.log(text);
+    const newFile = new File(
+      [
+        text
+      ],
+      file.name,
+      {
+        type: file.type,
+        lastModified: new Date(Date.now())
+      }
+    );
+
+    write(newFile)
+  }
+
   return (
     <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/plain</i>
+      <TextField
+        value={text}
+        onChange={handleChange}
+        multiline
+        rows={5}
+        size="medium"
+        rowsMax={7}
+      />
     </div>
-  );
+  )
+
 }
 
 PlaintextEditor.propTypes = {
